c0a111e2515dc208456dbf6c570363777d74cef4
seunje <karunce@gmail.com>
2020-09-26 12:48:35 +0100

    Initial

---
+++ Assets/SquareScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SquareScript : MonoBehaviour
+{
+    // Start isp called before the first frame update
+    Animator animator;
+    void Start()
+    {
+        
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+            animator.SetTrigger("To1");
+        if (Input.GetKeyUp(KeyCode.S))
+            animator.SetTrigger("To2");
+        
+    }
+}
