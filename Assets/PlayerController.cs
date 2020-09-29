65e09ee7307220bce7d16bf45f50a1918ba14bd4
seunje <karunce@gmail.com>
2020-09-29 19:24:09 +0100

    More rays in PlayerController

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -22,22 +22,123 @@ public class PlayerController : MonoBehaviour
             velocity = velocity + gravity * Time.fixedDeltaTime;
         }
         Vector2 d = velocity * Time.fixedDeltaTime;
-
-        Vector2 ray = new Vector2(boxCollider.bounds.min.x, boxCollider.bounds.min.y);
-        Vector2 dir = Vector2.down;
-        float dist = Mathf.Abs(d.y);
-
-        RaycastHit2D hit = Physics2D.Raycast(ray, dir, dist, groundMask);
-        Debug.DrawRay(ray, dir * dist, Color.red);
-
-        if (hit.collider && hit.distance > 0.0f)
+        if (Input.GetKey(KeyCode.D))
         {
-            velocity = new Vector2(velocity.x, 0.0f);
-            d = new Vector2(d.x, dir.y * hit.distance);
-            isGrounded = true;
+            d = d + new Vector2(Time.fixedDeltaTime * 2.0f, 0.0f);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            d = d + new Vector2(-Time.fixedDeltaTime * 2.0f, 0.0f);
         }
 
-        transform.position = transform.position + new Vector3(d.x, d.y, 0.0f);
+        if (d.y < 0.0)
+        {
+ 
+            // ray bottom left -> down
+            {
+                Vector2 ray = new Vector2(boxCollider.bounds.min.x, boxCollider.bounds.min.y);
+                Vector2 dir = Vector2.down;
+                float dist = Mathf.Abs(d.y);
 
+                RaycastHit2D hit = Physics2D.Raycast(ray, dir, dist, groundMask);
+                Debug.DrawRay(ray, dir * dist, Color.red);
+
+                if (hit.collider)
+                {
+                    velocity = new Vector2(velocity.x, 0.0f);
+                    d = new Vector2(d.x, Mathf.Sign(d.y) * hit.distance);
+
+                    isGrounded = true;
+                }
+            }
+
+            // ray bottom right -> down
+            {
+                Vector2 ray = new Vector2(boxCollider.bounds.max.x, boxCollider.bounds.min.y);
+                Vector2 dir = Vector2.down;
+                float dist = Mathf.Abs(d.y);
+
+                RaycastHit2D hit = Physics2D.Raycast(ray, dir, dist, groundMask);
+                Debug.DrawRay(ray, dir * dist, Color.red);
+
+                if (hit.collider)
+                {
+                    velocity = new Vector2(velocity.x, 0.0f);
+                    d = new Vector2(d.x, Mathf.Sign(d.y) * hit.distance);
+                    isGrounded = true;
+                }
+            }
+        }
+        if (d.x > 0.0)
+        {
+
+            // ray top right -> right
+            {
+                Vector2 ray = new Vector2(boxCollider.bounds.max.x, boxCollider.bounds.max.y);
+                Vector2 dir = Vector2.right;
+                float dist = Mathf.Abs(d.x);
+
+                RaycastHit2D hit = Physics2D.Raycast(ray, dir, dist, groundMask);
+                Debug.DrawRay(ray, dir, Color.red);
+
+                if (hit.collider)
+                {
+                    print(hit.distance);
+                    d = new Vector2(Mathf.Sign(d.x) * hit.distance, d.y);
+                }
+            }
+
+            // ray bottom right -> right
+            {
+                Vector2 ray = new Vector2(boxCollider.bounds.max.x, boxCollider.bounds.min.y);
+                Vector2 dir = Vector2.right;
+                float dist = Mathf.Abs(d.x);
+
+                RaycastHit2D hit = Physics2D.Raycast(ray, dir, dist, groundMask);
+                Debug.DrawRay(ray, dir, Color.red);
+
+                if (hit.collider)
+                {
+                    d = new Vector2(Mathf.Sign(d.x) * hit.distance, d.y);
+                }
+            }
+        }
+        if (d.x < 0.0)
+        {
+
+            // ray top left -> left
+            {
+                Vector2 ray = new Vector2(boxCollider.bounds.min.x, boxCollider.bounds.max.y);
+                Vector2 dir = Vector2.left;
+                float dist = Mathf.Abs(d.x);
+
+                RaycastHit2D hit = Physics2D.Raycast(ray, dir, dist, groundMask);
+                Debug.DrawRay(ray, dir, Color.red);
+
+                if (hit.collider)
+                {
+                    print(hit.distance);
+                    d = new Vector2(Mathf.Sign(d.x) * hit.distance, d.y);
+                }
+            }
+
+            // ray bottom left -> left
+            {
+                Vector2 ray = new Vector2(boxCollider.bounds.min.x, boxCollider.bounds.min.y);
+                Vector2 dir = Vector2.left;
+                float dist = Mathf.Abs(d.x);
+
+                RaycastHit2D hit = Physics2D.Raycast(ray, dir, dist, groundMask);
+                Debug.DrawRay(ray, dir, Color.red);
+
+                if (hit.collider)
+                {
+                    d = new Vector2(Mathf.Sign(d.x) * hit.distance, d.y);
+                }
+            }
+        }
+
+
+        transform.position = transform.position + new Vector3(d.x, d.y, 0.0f);
     }
 }
